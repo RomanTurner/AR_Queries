65123fa1276444dd7d1191d193416cf417d5a1dc
Roman Turner <rn.turner@live.com>
2021-07-28 15:49:35 -0700

    chachacha changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-=begin
-git add . 
-git commit -m 'practice'
-git push
-
-=end
